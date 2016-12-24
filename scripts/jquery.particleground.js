952df864ef806f248887b17bf4f7accf7c10d455
vikas-0 <vikas@geekyhub.in>
2016-12-24 16:24:09 +0530

    upd

--- scripts/jquery.particleground.js
+++ scripts/jquery.particleground.js
@@ -373,19 +373,19 @@
   };
 
   window[pluginName].defaults = {
-    minSpeedX: 0.1,
-    maxSpeedX: 0.7,
-    minSpeedY: 0.1,
-    maxSpeedY: 0.7,
+    minSpeedX: 0.1*2,
+    maxSpeedX: 0.7*2,
+    minSpeedY: 0.1*2,
+    maxSpeedY: 0.7*2,
     directionX: 'center', // 'center', 'left' or 'right'. 'center' = dots bounce off edges
     directionY: 'center', // 'center', 'up' or 'down'. 'center' = dots bounce off edges
     density: 10000, // How many particles will be generated: one particle every n pixels
     dotColor: '#666666',
     lineColor: '#666666',
-    particleRadius: 7, // Dot size
+    particleRadius: 4, // Dot size
     lineWidth: 1,
     curvedLines: false,
-    proximity: 100, // How close two dots need to be before they join
+    proximity: 50, // How close two dots need to be before they join
     parallax: true,
     parallaxMultiplier: 5, // The lower the number, the more extreme the parallax effect
     onInit: function() {},
